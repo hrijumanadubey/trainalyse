64b8874dfe7235e4576ca860e44a954003680a09
Hrijumana Dubey <hriju@Hrijumanas-MacBook-Air.local>
2026-03-31 15:11:16 +0530

    added + and - button for drop sets, corrected the bug for entering the values in drop sets,initially there is one input available before you click the + button

--- src/Day.jsx
+++ src/Day.jsx
@@ -1,62 +1,15 @@
 import React from "react";
-
+import Set from "./Set";
 import { useNavigate } from "react-router-dom";
 
 function Day() {
-  let ui;
   const navigate = useNavigate();
 
   const [date, setDate] = React.useState(""); //state for setting date
   const [edit, setEdit] = React.useState(false); //state for editing date
   const [day, setDay] = React.useState(""); //state for naming the day eg.chest day
   const [exerciseName, setExerciseName] = React.useState(""); //state for setting exercise name
-  const [weight, setWeight] = React.useState(""); //state for setting weights
-  const [reps, setReps] = React.useState(""); //state for setting reps
-  const [minutes, setMinutes] = React.useState("0"); //state for setting minutes
-  const [seconds, setSeconds] = React.useState("0"); //state for setting seconds
   const [exerciseType, setExerciseType] = React.useState(""); //state for setting the type of exercise eg.weights and reps,duration or assisted etc
-
-  if (exerciseType === "weightsAndReps") {
-    ui = (
-      <>
-        {" "}
-        <input
-          type="number"
-          placeholder=" enter Weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder=" enter Reps"
-          value={reps}
-          onChange={(e) => setReps(e.target.value)}
-        />
-      </>
-    );
-  } else if (exerciseType === "duration") {
-    ui = (
-      <>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={minutes}
-          onChange={(e) => setMinutes(e.target.value)}
-        />
-        <label>{minutes} : Minutes</label>
-        <br />
-        <input
-          type="range"
-          min="0"
-          max="59"
-          value={seconds}
-          onChange={(e) => setSeconds(e.target.value)}
-        />
-        <label>{seconds} : Seconds</label>
-      </>
-    );
-  }
 
   return (
     <>
@@ -105,19 +58,7 @@ function Day() {
         onChange={(e) => setExerciseName(e.target.value)}
       />
       <br />
-      <p>Set 1:</p>
-      <select
-        value={exerciseType}
-        onChange={(e) => setExerciseType(e.target.value)}
-      >
-        <option value="" disabled>
-          Select a Type
-        </option>
-        <option value="weightsAndReps">Weights and Reps</option>
-        <option value="duration">Duration</option>
-      </select>
-      <br />
-      {ui}
+      <Set exerciseType={exerciseType} setExerciseType={setExerciseType} />
     </>
   );
 }
